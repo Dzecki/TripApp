af9ee889440547fa69414ac2eddf2596fb07b087
Dzecki <oliwier.kostrzewski@wp.pl>
2023-03-09 13:59:02 +0100

    template

--- axi_trip/src/App.js
+++ axi_trip/src/App.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1 className=''>React</h1>
+    <div className='flex w-[100vw] h-[100vh] bg-[#252222]'>
+      <div className='flex justify-center w-[75%]'>
+
+        <div className='w-[1000px] flex flex-col justify-center my-8'>
+
+          /* Main window with information display
+          
+          <div className='flex bg-[#6D6F87] h-[650px] mt-4 mb-2 rounded-md'>
+            <div className='w-[290px] h-[300px] bg-[#252222] text-white font-bold text-center rounded-2xl my-12 ml-12'></div>
+            <div className='w-[290px] h-[300px] bg-[#252222] text-white font-bold text-center rounded-2xl m-12'></div>
+            <div>
+              <div className='w-[225px] h-[145px] bg-[#252222] mt-12 mb-2 rounded-2xl'></div>
+              <div className='w-[225px] h-[145px] bg-[#252222] mb-2 rounded-2xl'></div>
+            </div>
+          </div>  
+
+          /* Menu for creating trips objects
+
+          <div className='h-[250px] bg-[#6D6F87] mb-4 mt-2 rounded-md'></div>
+        </div>
+
+        /* Side menu with trips objects
+
+        <div className='self-center w-[250px] h-[865px] mx-4 my-8 rounded-md bg-[#6D6F87]'>
+
+        </div>
+      </div>
+
+      <div className='w-[25%]'>
+        
+      </div>
     </div>
   )
 }
