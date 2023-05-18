a15554ada585fe3e975a6544d4ffe1df7fd89a11
Dzecki <oliwier.kostrzewski@wp.pl>
2023-05-18 14:04:10 +0200

    commit nothing

--- axi_trip/src/App.js
+++ axi_trip/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 import { CirclePicker } from 'react-color';
-import Sketch from './Images/Sketch.png';
-import Sketch2 from './Images/Sketch2.png';
 import TripItem from './Components/TripItem';
 import PassengerItemList from './Components/PassengerItemList';
 
@@ -10,7 +8,7 @@ function App() {
   const [opacValue, setOpacValue] = useState(0);
 
   return (
-    <div className='flex w-[100vw] h-[100vh] bg-[#252222]'>
+    <div className='flex justify-center w-[100vw] h-[100vh] bg-[#252222]'>
       <div className='flex justify-center ml-24 w-[75%]'>
 
         <div className='w-[1000px] flex flex-col justify-center my-8'>
@@ -27,50 +25,40 @@ function App() {
               </div>
             </div>
 
-            <div id='tripNotes' className='flex ml-12 mb-4 text-white'>
+            {/* <div id='tripNotes' className='flex ml-12 mb-4 text-white'>
               <PassengerItemList/>
               <PassengerItemList/>
               <PassengerItemList/>
               <PassengerItemList/>
-            </div>
-          </div>  
+            </div> */}
+          </div>
 
           {/* Menu for creating trips objects */}
 
           <div id='createMenu' className='flex relative h-[250px] bg-[#6D6F87] mb-4 mt-4 rounded-md overflow-hidden'>
-
-              <button className='absolute top-2 right-2 z-20' onClick={() => setOpacValue(opacValue ? 0:100)}>X</button>
-              <div className='absolute w-[100%] h-[50%] bg-gray-800 transition-opacity z-10 top-0' style={{opacity: opacValue}}></div>
-
               <CirclePicker className='overflow-hidden bg-[#252222] bg-opacity-90 pt-2 pl-6'
                 color={pickedColor}
                 onChange={(color) => {
                   setPickedColor(color.hex);
                 }}
               />
-              <div style={{backgroundColor: pickedColor}} className='w-[50px] border-r-[1px] border-gray-700 transition-colors'></div>
+              <div style={{backgroundColor: pickedColor}} className='w-[40px] border-r-[1px] border-gray-700 transition-colors'></div>
 
-              <div className='w-[180px] h-[180px] bg-[#252222] m-5 ml-12 rounded-md bg-opacity-90 '></div> 
+              <div className='w-[180px] h-[180px] bg-[#252222] m-5 ml-12 rounded-md'></div> 
               <div className='m-5'>
-                <div className='w-[180px] h-[80px] bg-[#252222] rounded-md bg-opacity-90'></div>
-                <div className='w-[180px] h-[80px] bg-[#252222] mt-5 rounded-md bg-opacity-90'></div>
+                <div className='w-[180px] h-[80px] bg-[#252222] rounded-md'></div>
+                <div className='w-[180px] h-[80px] bg-[#252222] mt-5 rounded-md'></div>
               </div>
-              <div className='w-[180px] h-[180px] bg-[#252222] m-5 rounded-md bg-opacity-90'></div> 
+              <div className='w-[180px] h-[180px] bg-[#252222] m-5 rounded-md'></div> 
           </div>
         </div>
 
         {/* Side menu with trips items displaying their content */}
 
         <div id='sideMenu' className='flex flex-col items-center justify-center self-center w-[220px] h-[865px] mx-12 my-8 rounded-md bg-[#6D6F87]'>
-          <TripItem pass={4} direction={"W - L"} hours={"12:45 - 14:05"}></TripItem>
+          <TripItem pass={4} color={pickedColor} direction={"W - L"} hours={"12:45 - 14:05"}></TripItem>
         </div>
       </div>
-
-      <div className='relative flex items-end justify-end mb-12 mr-24 w-[20%]'>
-        <h1 style={{fontFamily: 'Sansation, sans-serif'}} className='absolute text-white text-[72px] top-0 right-2'>A X I</h1>
-        <img src={Sketch2}></img>
-        <img src={Sketch} className="mx-2 mt-8 h-[85%]"></img>
-      </div> 
     </div>
   )
 }
--- axi_trip/src/Components/TripItem.js
+++ axi_trip/src/Components/TripItem.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-function TripItem({direction, hours, pass}) {
+function TripItem({direction, hours, pass, color}) {
   return (
-    <div className='flex flex-col pt-4 items-center text-white font-bold w-[170px] h-[150px] bg-gray-600 my-6 rounded-md hover:bg-opacity-90 transition-shadow shadow-xl cursor-pointer'>
-        <h1 className='h-[33%] text-2xl'>{direction}</h1>
-        <h2 className='h-[34%] text-lg'>{hours}</h2>
-        <h1 className='h-[33%] text-xl'>PASS: {pass}</h1>
+    <div className='flex flex-col items-center text-white font-bold w-[170px] h-[150px] bg-[#252222] my-6 rounded-md hover:bg-opacity-90 transition-shadow shadow-xl cursor-pointer transition-all'>
+        <div className='flex items-center justify-around h-[10%] p-4 w-[100%] m-1'>
+          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
+          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
+          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
+          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
+        </div>
+        <h1 className='h-[30%] text-2xl'>{direction}</h1>
+        <h2 className='h-[30%] text-lg'>{hours}</h2>
+        <h1 className='h-[30%] text-xl'>PASS: {pass}</h1>
     </div>
   )
 }
