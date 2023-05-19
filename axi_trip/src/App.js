c310902860f4ba48f161974e610b08dba5add467
Dzecki <oliwier.kostrzewski@wp.pl>
2023-05-19 14:04:16 +0200

    display

--- axi_trip/src/App.js
+++ axi_trip/src/App.js
@@ -4,8 +4,30 @@ import TripItem from './Components/TripItem';
 import PassengerItemList from './Components/PassengerItemList';
 
 function App() {
-  const [pickedColor, setPickedColor] = useState("#3f51b5");
-  const [opacValue, setOpacValue] = useState(0);
+  const [tripsList, setTripsList] = useState([]);
+  const [trip, setTrip] = useState({ID: tripsList.length, From: '', To: '', dateStart: '', dateEnd: '', Color: '#3f51b5', Pass: 0});
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setTrip({
+      ...trip,[e.target.name]: value
+    })
+  }
+
+  const addTrip = () => {
+    setTripsList([...tripsList, trip]);
+    setTrip(false);
+    setTrip({...trip, ID: tripsList.length+1});
+  }
+
+  const deleteTrip = (id) => {  
+    const newList = tripsList.filter((trip) => id !== trip.ID);
+    setTripsList(newList);
+  } 
+
+  const tripsDisplay = tripsList.map((trip) => (
+    <TripItem {...trip} deleteTrip={deleteTrip}></TripItem>
+  ));
 
   return (
     <div className='flex justify-center w-[100vw] h-[100vh] bg-[#252222]'>
@@ -17,7 +39,9 @@ function App() {
           
           <div id='main' className='flex flex-col bg-[#6D6F87] h-[700px] mt-4 mb-4 rounded-md'>
             <div id='tripMenu' className='flex'>
-              <div className='w-[290px] h-[300px] bg-[#252222] text-white font-bold text-center rounded-2xl my-12 ml-12 shadow-lg'></div>
+              <div className='w-[290px] h-[300px] bg-[#252222] text-white font-bold text-center rounded-2xl my-12 ml-12 shadow-lg'>
+                Siema
+              </div>
               <div className='w-[290px] h-[300px] bg-[#252222] text-white font-bold text-center rounded-2xl m-12 shadow-lg'></div>
               <div>
                 <div className='w-[225px] h-[145px] bg-[#252222] mt-12 mb-2 rounded-2xl shadow-lg'></div>
@@ -35,28 +59,46 @@ function App() {
 
           {/* Menu for creating trips objects */}
 
-          <div id='createMenu' className='flex relative h-[250px] bg-[#6D6F87] mb-4 mt-4 rounded-md overflow-hidden'>
-              <CirclePicker className='overflow-hidden bg-[#252222] bg-opacity-90 pt-2 pl-6'
-                color={pickedColor}
+          <form id='createMenu' className='flex relative h-[250px] bg-[#6D6F87] mb-4 mt-4 rounded-md overflow-hidden'>
+              <CirclePicker className='overflow-hidden bg-[#252222] bg-opacity-90 pt-2 pl-6' color={trip.Color} 
                 onChange={(color) => {
-                  setPickedColor(color.hex);
+                  setTrip({...trip, Color: color.hex});
                 }}
               />
-              <div style={{backgroundColor: pickedColor}} className='w-[40px] border-r-[1px] border-gray-700 transition-colors'></div>
+              <div style={{backgroundColor: trip.Color}} className='w-[40px] border-r-[1px] border-gray-700 transition-colors'></div>
 
-              <div className='w-[180px] h-[180px] bg-[#252222] m-5 ml-12 rounded-md'></div> 
+              <div className='relative flex flex-col text-white justify-center items-center w-[180px] h-[180px] bg-[#252222] my-5 mr-2 ml-10 rounded-md'>
+                <label className='absolute top-2 text-[12px] font-bold'>START DATE</label>
+                <input className='bg-transparent font-bold text-2xl w-[160px] outline-none' type='date' name='startDate' onChange={handleChange}></input>  
+              </div> 
               <div className='m-5'>
-                <div className='w-[180px] h-[80px] bg-[#252222] rounded-md'></div>
-                <div className='w-[180px] h-[80px] bg-[#252222] mt-5 rounded-md'></div>
+                <div className='w-[180px] h-[80px] bg-[#252222] rounded-md text-white'>
+                  <label className='text-[12px] font-semibold ml-2'>FROM</label>
+                  <input className='w-[90%]  outline-none bg-transparent font-bold ml-2 mt-3 border-b border-white' name='From' onChange={handleChange}></input>
+                </div>
+                <div className='w-[180px] h-[80px] bg-[#252222] text-white mt-5 rounded-md'>
+                  <label className='text-[12px] font-semibold ml-2'>TO</label>
+                  <input className='w-[90%]  outline-none bg-transparent font-bold ml-2 mt-3 border-b border-white' name='To' onChange={handleChange}></input>
+                </div>
               </div>
-              <div className='w-[180px] h-[180px] bg-[#252222] m-5 rounded-md'></div> 
-          </div>
+              <div className='relative flex flex-col justify-center items-center text-white w-[180px] h-[180px] bg-[#252222] my-5 ml-2 mr-5 rounded-md'>
+                <label className='absolute top-2 text-[12px] font-bold'>END DATE</label>
+                <input className='bg-transparent font-bold text-2xl w-[160px] outline-none' type='date' name='endDate' onChange={handleChange}></input>   
+              </div> 
+              <div className='flex h-[180px] mt-5 flex-col justify-between items-center mr-2 w-[80px]'>
+                <div className='flex flex-col justify-center items-center w-[100%] h-[120px] text-center bg-[#252222] text-white rounded-md'>
+                  <label className='text-[11px] font-semibold mb-5'>N.O. PASSENGERS</label>
+                  <input type='number' placeholder='1' min={1} max={6} name='Pass' onChange={handleChange} className='bg-transparent w-[50%] outline-none ml-7'></input>
+                </div>
+                <button className='bg-[#252222] rounded-md font-bold p-2 text-white w-[100%] hover:opacity-95 transition-all' onClick={addTrip} type='reset' >ADD</button>
+              </div>
+          </form>
         </div>
 
         {/* Side menu with trips items displaying their content */}
 
-        <div id='sideMenu' className='flex flex-col items-center justify-center self-center w-[220px] h-[865px] mx-12 my-8 rounded-md bg-[#6D6F87]'>
-          <TripItem pass={4} color={pickedColor} direction={"W - L"} hours={"12:45 - 14:05"}></TripItem>
+        <div id='sideMenu' className='flex flex-col items-center justify-center self-center w-[220px] h-[865px] mx-12 my-8 rounded-md bg-[#6D6F87] overflow-scroll scrollbar-hide'>
+          {tripsDisplay}
         </div>
       </div>
     </div>
--- axi_trip/src/Components/TripItem.js
+++ axi_trip/src/Components/TripItem.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
-function TripItem({direction, hours, pass, color}) {
+function TripItem({ID, From, To, startDate, endDate, Color, Pass, deleteTrip}) {
   return (
-    <div className='flex flex-col items-center text-white font-bold w-[170px] h-[150px] bg-[#252222] my-6 rounded-md hover:bg-opacity-90 transition-shadow shadow-xl cursor-pointer transition-all'>
+    <div className='relative flex flex-col items-center text-white font-bold w-[170px] h-[150px] bg-[#252222] my-6 rounded-md transition-shadow shadow-xl cursor-pointer transition-all'>
         <div className='flex items-center justify-around h-[10%] p-4 w-[100%] m-1'>
-          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
-          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
-          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
-          <div style={{backgroundColor: color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
+          <div style={{backgroundColor: Color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
+          <button className='rounded-full w-[20px] h-[20px] text-[11px] font-bold bg-red-500 hover:bg-red-600 transition-color' onClick={() => deleteTrip(ID)}>X</button>
+          <div style={{backgroundColor: Color}} className='bg-purple-600 w-[13px] h-[13px] rounded-full'></div>
         </div>
-        <h1 className='h-[30%] text-2xl'>{direction}</h1>
-        <h2 className='h-[30%] text-lg'>{hours}</h2>
-        <h1 className='h-[30%] text-xl'>PASS: {pass}</h1>
+        <h1 className='h-[30%] text-2xl'>{From.charAt(0).toUpperCase()} - {To.charAt(0).toUpperCase()}</h1>
+        <h1 className='h-[30%] text-xl'>N.O.P : {Pass}</h1>
+        <h1 className='h-[30%] text-[13px]'>{startDate} / {endDate}</h1>
     </div>
   )
 }
